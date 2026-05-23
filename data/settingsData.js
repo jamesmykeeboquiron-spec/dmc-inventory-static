402183300c306d327c293da0892a36c1c9dc2f95
jamesmykeeboquiron-spec <jamesmykeeboquiron@gmail.com>
2026-05-22 20:27:19 -0700

    Add settings dropdown data

---
+++ data/settingsData.js
@@ -0,0 +1,70 @@
+window.DMC_DATA = window.DMC_DATA || {};
+
+window.DMC_DATA.settings = {
+  operatingAreas: [
+    {
+      id: "branch-station",
+      name: "Branch/Station"
+    },
+    {
+      id: "commissary",
+      name: "Commissary"
+    }
+  ],
+
+  departments: [
+    {
+      id: "bar",
+      name: "Bar",
+      operatingAreaId: "branch-station"
+    },
+    {
+      id: "kitchen",
+      name: "Kitchen",
+      operatingAreaId: "branch-station"
+    },
+    {
+      id: "dining",
+      name: "Dining",
+      operatingAreaId: "branch-station"
+    },
+    {
+      id: "commissary",
+      name: "Commissary",
+      operatingAreaId: "commissary"
+    }
+  ],
+
+  sections: [
+    {
+      id: "bar-coffee",
+      name: "Coffee",
+      departmentId: "bar"
+    },
+    {
+      id: "bar-milk",
+      name: "Milk",
+      departmentId: "bar"
+    },
+    {
+      id: "bar-syrups",
+      name: "Syrups",
+      departmentId: "bar"
+    },
+    {
+      id: "bar-powders",
+      name: "Powders",
+      departmentId: "bar"
+    },
+    {
+      id: "bar-cups-lids",
+      name: "Cups & Lids",
+      departmentId: "bar"
+    },
+    {
+      id: "bar-supplies",
+      name: "Bar Supplies",
+      departmentId: "bar"
+    }
+  ]
+};
